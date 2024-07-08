c4c8dfa32f653cb22eb318659441b0faa7b83a34
Evy <evytchr@gmail.com>
2024-07-08 18:11:15 +0200

    feat(style) : style des badges

--- src/components/Badge/index.jsx
+++ src/components/Badge/index.jsx
@@ -1,11 +1,12 @@
 import BadgeBootstrap from 'react-bootstrap/Badge';
 import "./index.scss"
 
-function Badge({title, picture}) {
+function Badge({title, picture, size}) {
+    const sizeClass = size === 'small' ? 'smallBadge' : "none";
     return (
-        <BadgeBootstrap className='badge' bg="secondary">
-            <img className="badge__picture" src={picture} alt={`logo de ${title}`}></img>
-            <p className="badge__title">{title}</p>
+        <BadgeBootstrap className={sizeClass} bg="secondary">
+            <img className={size === 'small' ? 'smallBadge__picture' : 'badge__picture'} src={picture} alt={`logo de ${title}`}></img>
+            <p className={size === 'small' ? 'smallBadge__title' : 'badge__title'}>{title}</p>
         </BadgeBootstrap>
     )
 }
--- src/components/MappedList/index.jsx
+++ src/components/MappedList/index.jsx
@@ -2,15 +2,15 @@ import Badge from '../../components/Badge'
 import Tab from '../../components/Tab'
 import './index.scss'
 
-function MappedList({data, type}) {
-    const SkillsList = ({ skills, type }) => (
+function MappedList({data, type,size}) {
+    const SkillsList = ({ skills, type, size }) => (
         <ul className={type === 'tab' ? "SkillsContainer__tabsList" : "SkillsContainer__badgesList"}>
         {skills.map((skill, index) => (
             <li key={`${skill.title}-${index}`} className={type === 'tab' ? "SkillsContainer__tabsList--skill" : "SkillsContainer__badgesList--skill"}>
             {type === 'tab' ? (
                 <Tab {...skill} />
             ) : (
-                <Badge {...skill} />
+                <Badge {...skill} size={size} />
             )}
             </li>
         ))}
@@ -20,7 +20,7 @@ function MappedList({data, type}) {
   return(
     
     <div className="SkillsContainer">
-        <SkillsList skills={data} type={type} />
+        <SkillsList skills={data} type={type} size={size}/>
     </div>
   )
 }
